5d76cd6612a9c12cc7517756422d44da1b6d2e24
Nam Le Xuan <lxnam@tma.com.vn>
2019-06-21 15:47:04 +0700

    update code

--- peer-server.js
+++ peer-server.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var PeerServer = require('peer').PeerServer;
+var axios = require('axios');
 
 var PORT = process.env.PORT || 9000;
 
@@ -8,6 +9,22 @@ var server = PeerServer({
     path: '/peerjs'
 });
 
+// Function clear peerID
+function clearPeerId(peeerId) {
+    axios({
+        method:'delete',
+        url: `https://tasm-authentication.herokuapp.com/api/v1/users/peer/${peeerId}`,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }).then(res => {
+        console.log(res);
+    }).catch(error => {
+        console.log('Error to clear peerID: ', error);
+    })
+}
+
 // Event peer
 server.on('connection', id => {
   console.log('connected on peer id:', id);
@@ -15,4 +32,5 @@ server.on('connection', id => {
 
 server.on('disconnect', async id => {
   console.log('disconnect peer id: ', id);
+  clearPeerId(id);
 });
